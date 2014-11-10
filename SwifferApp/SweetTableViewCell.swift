b376cd805ca6c002324edbbb3129739e39eec7ca
kkhattab <kareemkhattab925@gmail.com>
2014-11-10 11:32:21 -0800

    App Like Twitter , Fixed all bugs up to Part 2

---
+++ SwifferApp/SweetTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  SweetTableViewCell.swift
+//  SwifferApp
+//
+//  Created by Kareem Khattab on 11/8/14.
+//  Copyright (c) 2014 Kareem Khattab. All rights reserved.
+//
+
+import UIKit
+
+class SweetTableViewCell: UITableViewCell {
+
+    @IBOutlet var usernameLabel: UILabel! = UILabel()
+    @IBOutlet var timestampLabel: UILabel! = UILabel()
+    @IBOutlet var sweetTextView: UITextView! = UITextView()
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+
+/*
+
+usernameLabel
+timestampLabel
+sweetTextView
+
+*/
---
+++ SwifferApp/TimeLineTableViewController.swift
@@ -0,0 +1,276 @@
+//
+//  TimeLineTableViewController.swift
+//  SwifferApp
+//
+//  Created by Kareem Khattab on 11/8/14.
+//  Copyright (c) 2014 Kareem Khattab. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineTableViewController: UITableViewController {
+
+    var timelineData = [PFObject]()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+    }
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    
+    @IBAction func loadData(){
+        timelineData.removeAll(keepCapacity: false)
+        
+            var findTimelineData:PFQuery = PFQuery(className:"Sweets")
+            findTimelineData.findObjectsInBackgroundWithBlock
+                {
+                    (objects:[AnyObject]! , error:NSError!) -> Void in
+                    if error == nil
+                    {
+                        self.timelineData = objects.reverse() as [PFObject]
+                        
+                        
+                        //let array:NSArray = self.timelineData.reverseObjectEnumerator().allObjects
+                    
+                        println(objects)
+                    
+                       // self.timelineData = array as NSMutableArray
+                    
+                    
+                        self.tableView.reloadData()
+                }
+            }
+    }
+
+    
+/*
+    @IBAction func loadData()
+    {
+        timelineData.removeAllObjects()
+        
+        var findTimelineData : PFQuery = PFUser.query()
+        findTimelineData.findObjectsInBackgroundWithBlock{
+            (objects: [AnyObject]!, error: NSError!)-> Void in
+            if error == nil
+            {
+                println("No error")
+                
+                if let objects = objects as? [PFObject!]
+                {
+                    for object in objects
+                    {
+                        self.timelineData.addObject(object)
+                    }
+                }
+                
+                let array : NSArray = self.timelineData.reverseObjectEnumerator().allObjects
+                self.timelineData = array as NSMutableArray
+                
+                self.tableView.reloadData()
+            }
+            
+        }
+    }
+
+*/
+    
+    override func viewDidAppear(animated: Bool) {
+        
+        self.loadData()
+        
+        
+        if((PFUser.currentUser()) == nil){
+            
+            var loginAlert:UIAlertController = UIAlertController(title: "Sign Up / Login ", message: "Please Sign Up or Login", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            loginAlert.addTextFieldWithConfigurationHandler({
+                textfield in
+                textfield.placeholder = "Your username"
+                
+            })
+            
+            loginAlert.addTextFieldWithConfigurationHandler({
+                textfield in
+                textfield.placeholder = "Your Password"
+                textfield.secureTextEntry = true
+                
+            })
+            
+            loginAlert.addAction(UIAlertAction(title: "Login", style: UIAlertActionStyle.Default, handler: {
+                alertAction in
+                let textFields:NSArray = loginAlert.textFields as AnyObject! as NSArray
+                let usernameTextField:UITextField = textFields.objectAtIndex(0) as UITextField
+                let passwordTextField:UITextField = textFields.objectAtIndex(1) as UITextField
+                
+                PFUser.logInWithUsernameInBackground(usernameTextField.text, password: passwordTextField.text){
+                    (user:PFUser!, error:NSError!)->Void in
+                    if((user) != nil){
+                        println("Login Successful")
+                    }else{
+                        println("Login Failed")
+                    }
+                }
+                
+                
+                
+            }))
+            
+            
+            
+            loginAlert.addAction(UIAlertAction(title: "Sign Up", style: UIAlertActionStyle.Default, handler: {
+                alertAction in
+                let textFields:NSArray = loginAlert.textFields as AnyObject! as NSArray
+                let usernameTextField:UITextField = textFields.objectAtIndex(0) as UITextField
+                let passwordTextField:UITextField = textFields.objectAtIndex(1) as UITextField
+                
+                var sweeter:PFUser = PFUser()
+                sweeter.username = usernameTextField.text
+                sweeter.password = passwordTextField.text
+                
+                sweeter.signUpInBackgroundWithBlock{
+                    (success:Bool!, error:NSError!)-> Void in
+                    
+                    if (error != nil) {
+                        
+                     println("Sign Up Successful")
+                    }else{
+                        
+                        println("Error")
+                        
+                    }
+                }
+                
+                
+            }))
+            
+            self.presentViewController(loginAlert, animated: true, completion: nil)
+ 
+        }
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        // #warning Potentially incomplete method implementation.
+        // Return the number of sections.
+        return 1
+    }
+
+ override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete method implementation.
+        // Return the number of rows in the section.
+        return timelineData.count
+    }
+
+
+
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell{
+        
+        let cell: SweetTableViewCell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as SweetTableViewCell
+        
+        //let sweet:PFObject = self.timelineData.objectAtIndex(indexPath.row) as PFObject
+        let sweet: PFObject = self.timelineData[indexPath.row] as PFObject
+        
+        
+        cell.sweetTextView.alpha = 0
+        cell.timestampLabel.alpha = 0
+        cell.usernameLabel.alpha = 0
+
+        
+        
+        cell.sweetTextView.text = sweet.objectForKey("content") as String
+        
+        
+        
+        var dataFormatter:NSDateFormatter = NSDateFormatter()
+        dataFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        cell.timestampLabel.text = dataFormatter.stringFromDate(sweet.createdAt)
+
+        
+        
+        var findSweeter: PFQuery = PFUser.query()
+        findSweeter.whereKey("objectId", equalTo: sweet.objectForKey("sweeter").objectId)
+        
+
+        
+        findSweeter.findObjectsInBackgroundWithBlock{
+            (objects:[AnyObject]!, error:NSError!)->Void in
+            if (error == nil){
+                let user:PFUser = (objects as NSArray).lastObject as PFUser
+                cell.usernameLabel.text = user.username
+                
+                UIView.animateWithDuration(0.5, animations: {
+                 cell.sweetTextView.alpha = 1
+                 cell.timestampLabel.alpha = 1
+                 cell.usernameLabel.alpha = 1 
+                })
+                
+            }
+        }
+        
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == .Delete {
+            // Delete the row from the data source
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
+        } else if editingStyle == .Insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        // Return NO if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using [segue destinationViewController].
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
